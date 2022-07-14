f3343bb3d0ebcd9cd5befac7bcfbd21b449a0395
Unknown Coder <ksy@vs21.kr>
2022-07-14 14:00:08 +0900

    초기 커밋.

---
+++ MainF.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rounded_edges_in_button
+{
+    public partial class MainF : Form
+    {
+        public MainF()
+        {
+            InitializeComponent();
+            ButtonRounded();
+        }
+
+        private void ButtonRounded()
+        {
+            Rectangle Rect = new Rectangle(0, 0, button1.Width, button1.Height);
+            GraphicsPath GraphPath = new GraphicsPath();
+
+            GraphPath.AddArc(Rect.X, Rect.Y, 50, 50, 180, 90);
+            GraphPath.AddArc(Rect.X + Rect.Width - 50, Rect.Y, 50, 50, 270, 90);
+            GraphPath.AddArc(Rect.X + Rect.Width - 50, Rect.Y + Rect.Height - 50, 50, 50, 0, 90);
+            GraphPath.AddArc(Rect.X, Rect.Y + Rect.Height - 50, 50, 50, 90, 90);
+
+            button1.Region = new Region(GraphPath);
+        }
+    }
+}
